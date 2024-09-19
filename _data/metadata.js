f8d7b361096e66be368f5f5be32b6babe4bc44ac
Len <len@astuanax.com>
2024-09-19 16:44:54 +0200

    Edits message.

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "UniversalAlt",
+	url: "https://universalalt.github.io/universal-alt/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "UniversalALt project",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Len Dierickx",
+		email: "len.dierickx@sitefig.eu",
+		url: "https://sitefig.eu/"
 	}
 }
